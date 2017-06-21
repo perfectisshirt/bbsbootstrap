7c7c123fc124e925f305a5106333cf98a674f45c
thatIsMe <perfectisshirt@users.noreply.github.com>
2017-06-21 11:19:13 -0400

    Add files via upload

--- Base.js
+++ Base.js
@@ -9,44 +9,6 @@ function Base(){
 	this.elements = [];
 }
 
-//添加class
-Base.prototype.addClass=function(className){
-	for(var i=0;i<this.elements.length;i++){
-		if(!hasClass(this.elements[i],className)){
-			this.elements[i].className += ' '+ className;
-		}
-		
-	}
-	return this;
-};
-
-//移除class
-Base.prototype.removeClass = function(className){
-	for(var i=0;i<this.elements.length;i++){
-		if(hasClass(this.elements[i],className)){
-			this.elements[i].className = this.elements[i].className.replace(new RegExp('(\\s|^)'+className+'(\\s|$)'),'');	
-		}
-		
-	}
-	return this;
-};
-
-//添加link或style的css规则,要小心使用，不能破坏整个css的结构
-Base.prototype.addRule = function(num,selectorText,cssText,position){
-	var sheet = document.styleSheets[num];
-	
-	insertRule(sheet,selectorText,cssText,position);
-	return this;
-};
-
-//移除link或style的css规则
-Base.prototype.removeRule=function(num,index){
-	var sheet = document.styleSheets[num];
-	deleteRule(sheet,index);
-	return this;
-};
-
-
 //获取id 节点
 Base.prototype.getId=function(id){
 	this.elements.push(document.getElementById(id));
@@ -99,6 +61,44 @@ Base.prototype.css = function (attr,value){
 	return this;
 }
 
+
+//添加class
+Base.prototype.addClass=function(className){
+	for(var i=0;i<this.elements.length;i++){
+		if(!hasClass(this.elements[i],className)){
+			this.elements[i].className += ' '+ className;
+		}
+		
+	}
+	return this;
+};
+
+//移除class
+Base.prototype.removeClass = function(className){
+	for(var i=0;i<this.elements.length;i++){
+		if(hasClass(this.elements[i],className)){
+			this.elements[i].className = this.elements[i].className.replace(new RegExp('(\\s|^)'+className+'(\\s|$)'),'');	
+		}
+		
+	}
+	return this;
+};
+
+//添加link或style的css规则,要小心使用，不能破坏整个css的结构
+Base.prototype.addRule = function(num,selectorText,cssText,position){
+	var sheet = document.styleSheets[num];
+	
+	insertRule(sheet,selectorText,cssText,position);
+	return this;
+};
+
+//移除link或style的css规则
+Base.prototype.removeRule=function(num,index){
+	var sheet = document.styleSheets[num];
+	deleteRule(sheet,index);
+	return this;
+};
+
 //设置innerHTML
 Base.prototype.html = function(str){
 	
@@ -111,28 +111,39 @@ Base.prototype.html = function(str){
 	return this;
 }
 
-//触发点击事件
-Base.prototype.click = function(fn){
+//设置鼠标移入移除方法
+Base.prototype.hover = function(over,out){
 	for(var i=0;i<this.elements.length;i++){
-		this.elements[i].onclick=fn;
-	}
-	return this;
-}
-
-//设置物体居中
-Base.prototype.center=function(width,height){
-	var top = (document.documentElement.clientHeight-250)/2;
-	var left = (document.documentElement.clientWidth-350)/2;
-	for(var i=0;i<this.elements.length;i++){
-		this.elements[i].style.top = top+'px';
-		this.elements[i].style.left = left+'px';
+		addEvent(this.elements[i],'mouseover',over);
+		addEvent(this.elements[i],'mouseout',out);
 	}
 	return this;
 };
 
-//触发浏览器窗口事件
-Base.prototype.resize = function(fn){
-	window.onresize=fn;
+//设置显示
+Base.prototype.show = function(){
+	for(var i=0;i<this.elements.length;i++){
+		this.elements[i].style.display = 'block';
+	}
+	return this;
+};
+
+//设置隐藏
+Base.prototype.hide = function(){
+	for(var i=0;i<this.elements.length;i++){
+		this.elements[i].style.display = 'none';
+	}
+	return this;
+};
+
+//设置物体居中
+Base.prototype.center=function(width,height){
+	var top = (getInner().height-250)/2;
+	var left = (getInner().width-350)/2;
+	for(var i=0;i<this.elements.length;i++){
+		this.elements[i].style.top = top+'px';
+		this.elements[i].style.left = left+'px';
+	}
 	return this;
 };
 
@@ -143,6 +154,7 @@ Base.prototype.lock=function(){
 		this.elements[i].style.height=getInner().height+'px';
 		this.elements[i].style.display='block';
 		document.documentElement.style.overflow="hidden";
+		
 	}
 	return this;
 };
@@ -157,22 +169,57 @@ Base.prototype.unlock=function(){
 	return this;
 };
 
+//触发点击事件
+Base.prototype.click = function(fn){
+	for(var i=0;i<this.elements.length;i++){
+		this.elements[i].onclick=fn;
+	}
+	return this;
+}
+
+//触发浏览器窗口事件
+Base.prototype.resize = function(fn){
+	for(var i=0;i<this.elements.length;i++){
+		var element = this.elements[i];
+        addEvent(window,'resize',function(){
+	              fn();
+				if(element.offsetLeft>getInner.width-element.offsetWidth){
+					element.style.left = getInner.width-element.offsetWidth+'px';
+				}
+				if(element.offsetTop>getInner.height-element.offsetHeight){
+					element.style.top = getInner.height-element.offsetHeight+'px';
+				}
+         });
+	}
+	return this;
+};
+
+
 //拖拽功能
 Base.prototype.drag=function(){
 	for(var i=0;i<this.elements.length;i++){
-		this.elements[i].onmousedown=function(e){
-			preDef(e);
-			var e = getEvent(e);
+		
+			addEvent(this.elements[i],'mousedown',function(e){
+					
+			if(trim(this.innerHTML).length==0)  e.preventDefault();
+			
 			var _this = this;
 			//得到实际的长度,offsetleft是整个物体到左边浏览器的距离
 			var diffX = e.clientX-_this.offsetLeft;
 			var diffY = e.clientY-_this.offsetTop;
-			//为IE特别设计的
-			if(typeof _this.setCapture!='undefined'){
-				_this.setCapture();
+			
+			//光标只有在h2区域的时候才能拖拽
+			if(e.target.tagName=='H2'){
+				addEvent(document,'mousemove',move);
+				addEvent(document,'mouseup',up);
+			}else{
+				removeEvent(document,'mousemove',move);
+				removeEvent(document,'mouseup',up);
 			}
-			document.onmousemove=function(e){
-				var e = getEvent(e);
+			
+				
+		
+		    function move(e){
 				var left = e.clientX - diffX;
 				var top = e.clientY  - diffY;
 				//左边拖出浏览器了
@@ -190,8 +237,16 @@ Base.prototype.drag=function(){
 				
 				_this.style.left = left + 'px';
 				_this.style.top = top + 'px';
-			}
-			document.onmouseup = function(){
+				
+				//为IE特别设计的
+				if(typeof _this.setCapture!='undefined'){
+					_this.setCapture();
+				}
+		    }
+			
+			function up(){
+				removeEvent(document,'mousemove',move);
+				removeEvent(document,'mouseup',up);
 				this.onmousemove=null;
 				this.onmouseup=null;
 				//为IE设计的
@@ -199,7 +254,7 @@ Base.prototype.drag=function(){
 					_this.releaseCapture();
 				}
 			}
-		};
+		});
 	}
 	return this;
 };
--- tool.js
+++ tool.js
@@ -48,39 +48,100 @@ function deleteRule(sheet,index){
 	}
 }
 
-//获取event对象
-function getEvent(event){
-	return event||window.event;
+// //获取event对象
+// function getEvent(event){
+// 	return event||window.event;
+// }
+// //阻止默认行为
+// function preDef(event){
+// 	var e = getEvent(event);
+// 	if(typeof e.preventDefault != 'undefined'){//W3C
+// 		e.preventDefault();
+// 	}else{  //IE
+// 		e.returnValue = false;
+// 	}
+// }
+
+//把IE常用的event对象配对到w3c中去
+addEvent.fixEvent = function (event){
+	event.preventDefault = addEvent.fixEvent.preventDefault;
+	event.stopPropagation = addEvent.fixEvent.stopPropagation;
+	event.target = event.srcElement;
+	return event;
+};
+
+
+//IE阻止默认行为
+addEvent.fixEvent.preventDefault = function(){
+	this.returnValue = false;
+};
+
+//IE取消冒泡
+addEvent.fixEvent.stopPropagation = function(){
+	this.cancelBubble=true;
+};
+	
+
+
+
+//为每个事件分配一个计数器实现累加，并且清晰的指出是专门给addEvent用的，js一切皆为对象，实际上是一个全局变量
+addEvent.ID = 1;
+//执行事件处理函数
+addEvent.exec = function(event){
+	var e = event||addEvent.fixEvent(window.event);
+	var es = this.events[e.type];
+	for(var i in es){
+		es[i].call(this,e);
 }
+};
 
-
-//阻止默认行为
-function preDef(event){
-	var e = getEvent(event);
-	if(typeof e.preventDefault != 'undefined'){//W3C
-		e.preventDefault();
-	}else{  //IE
-		e.returnValue = false;
+//同一个注册函数进行屏蔽
+addEvent.equal = function(es,fn){
+	for(var i in es){
+		if(es[i]==fn) return true;
 	}
-}
-
-
+	return false;
+};
 //跨浏览器绑定事件
 function addEvent(obj,type,fn){
 	if(typeof obj.addEventListener !='undefined'){//W3C
 		obj.addEventListener(type,fn,false);
-	}else if(typeof obj.attachEvent!='undefined'){//IE
-		obj.attachEvent('on'+type,function(){
-			fn.call(obj，window.event);   //对象冒充
-		});
+	}else {
+		//创建一个存放事件的哈希表
+      	if(!obj.events) obj.events={};
+		if(!obj.events[type]){
+			//创建一个存放事件的数组
+			obj.events[type]=[];
+			//把第一次的事件处理函数储存到第一个位置上
+			if(obj['on'+type]) obj.events[type][0]=fn;
+		}else{
+			//同一个注册函数进行屏蔽，不添加到计数器中
+			if(addEvent.equal(obj.events[type],fn)) return false;	
+		}
+		//从第二次开始我们用事件计数器来存储
+		obj.events[type][addEvent.ID++]=fn;
+		//执行事件处理函数
+		obj['on'+type] = addEvent.exec;
 	}
+	
 }
 
 //跨浏览器删除事件
 function removeEvent(obj,type,fn){
 	if(typeof obj.removeEventListener!='undefined'){
 		obj.removeEventListener(type,fn,false);
-	}else if(typeof obj.detachEvent!='undefined'){
-		obj.detachEvent('on'+type,fn);
+	}else {
+		if(obj.events){
+		for(var i in obj.events[type]){
+			if(obj.events[type][i]==fn){
+				delete obj.events[type][i];
+			}
+		}
 	}
+	}
+}
+
+//删除左右空格
+function trim(str){
+	return str.replace(/(^\s*)|(\s*$)/g,'');
 }
